3c0dffbdea1a390b9765a6a75fe5d9d0f28cfc63
furkaneryilmazz <133990378+furkaneryilmazz@users.noreply.github.com>
2024-07-29 16:59:53 +0300

    CSS-Update

--- KitchenSecrets/wwwroot/js/site.js
+++ KitchenSecrets/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+    var cards = document.querySelectorAll('.card-custom');
+    var maxHeight = 0;
+
+    // Kartların maksimum yüksekliğini bul
+    cards.forEach(function(card) {
+        card.style.height = 'auto'; // Yüksekliği sıfırla
+    var height = card.offsetHeight;
+        if (height > maxHeight) {
+        maxHeight = height;
+        }
+    });
+
+    // Kartların yüksekliğini eşitle
+    cards.forEach(function(card) {
+        card.style.height = maxHeight + 'px';
+    });
+});
+
+    window.addEventListener('resize', function () {
+    var cards = document.querySelectorAll('.card-custom');
+    var maxHeight = 0;
+
+    // Kartların maksimum yüksekliğini bul
+    cards.forEach(function(card) {
+        card.style.height = 'auto'; // Yüksekliği sıfırla
+    var height = card.offsetHeight;
+        if (height > maxHeight) {
+        maxHeight = height;
+        }
+    });
+
+    // Kartların yüksekliğini eşitle
+    cards.forEach(function(card) {
+        card.style.height = maxHeight + 'px';
+    });
+});
+</script>
